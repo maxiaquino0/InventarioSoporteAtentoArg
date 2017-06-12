1652bdf08b3084bfd9f8a9465c5d1c6ab603a06e
Maximiliano Aquino <maxiaquino0@gmail.com>
2017-06-12 08:49:17 -0300

    Add project files.

---
+++ InventarioSoporteAtentoArg/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InventarioSoporteAtentoArg.Startup))]
+namespace InventarioSoporteAtentoArg
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
